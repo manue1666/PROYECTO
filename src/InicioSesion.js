dbfa6de1234bb9d07b703ecea7242f43ae64bb8d
JavierMora2 <JavierMora2@github>
2024-10-23 13:37:24 -0600

    cambio InicioSesion

--- src/InicioSesion.js
+++ src/InicioSesion.js
@@ -1,34 +1,32 @@
 import React, { useState } from "react";
-import { Button, Form, Container,Card, Navbar, Nav } from "react-bootstrap";
+import { Button, Form, Container, Card, Navbar, Nav } from "react-bootstrap";
 
 const InicioSesion = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  /* Función para manejar el envío del formulario*/
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*  Evita que la página se recargue
-     */
-    /* Simulación de la lógica de inicio de sesión */
+
     if (email === "pin@example.com" && password === "password") {
-      console.log("Inicio de sesión exitoso"); /*  Mensaje de éxito */
+      console.log("Inicio de sesión exitoso");
     } else {
-      console.error("Error: Credenciales incorrectas"); /* Maneja los errores */
+      console.error("Error: Credenciales incorrectas");
     }
   };
 
   return (
-
-    <><Navbar bg="dark" variant="dark">
-      <Container >
-        <Navbar.Brand href="/">Cuestionarios</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/inicio_sesion">Login</Nav.Link>
-          <Nav.Link href="/sing_in">regist</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar><Container className="mt-4">
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Cuestionarios</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/inicio_sesion">Login</Nav.Link>
+            <Nav.Link href="/sing_in">regist</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container className="mt-4">
         <Card>
           <Card.Body>
             <Card.Title>Inicia sesion</Card.Title>
@@ -39,8 +37,9 @@ const InicioSesion = () => {
                   placeholder="Ingresa un correo electrónico"
                   type="email"
                   name="email"
-                  onChange={(e) => setEmail(e.target.value)} /* Actualiza el estado del email */
-                  required />
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
 
               <div className="mt-3">
@@ -49,18 +48,26 @@ const InicioSesion = () => {
                   placeholder="Ingresa tu contraseña"
                   type="password"
                   name="password"
-                  onChange={(e) => setPassword(e.target.value)} /* Actualiza el estado de la contraseña */
-                  required />
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
               </div>
 
               <Button className="mt-3" type="submit">
                 Ingresar
               </Button>
-              <Button href="/recover_password" className="position-absolute buttom-0 end-0 mt-3 m-3" variant="outline-primary" >Olvide mi contaseña</Button>
+              <Button
+                href="/recover_password"
+                className="position-absolute buttom-0 end-0 mt-3 m-3"
+                variant="outline-primary"
+              >
+                Olvide mi contaseña
+              </Button>
             </Form>
           </Card.Body>
         </Card>
-      </Container></>
+      </Container>
+    </>
   );
 };
 
