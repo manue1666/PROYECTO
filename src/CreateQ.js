fa048b2dd779ca7602d79a0f057797beb5123e01
DanielArellano31 <utm23090410@utma.edu.mx>
2024-11-10 00:11:44 -0600

    Funcion para graficar, sin poder graficar hasta que las validaciones esten listas

--- src/CreateQ.js
+++ src/CreateQ.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { Card, Container, Form, Button, InputGroup } from 'react-bootstrap';
 import { AnswerQuestionnaire } from "./components/AnwerQuestionnaire";
 import { Barra } from "./Navbar";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 const CreateQ = () => {
@@ -84,6 +87,34 @@ const CreateQ = () => {
     data.preguntas[pIndex].opciones= borrarO;
     setCreateQ(data)
   }
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
+ const data = {
+  labels: ["opciones"],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [{ onChangeOption}],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
   return (
     <><Barra /><Container>
@@ -164,6 +195,13 @@ const CreateQ = () => {
 
       <h3 className="mt-4">Vista Previa</h3>
       <AnswerQuestionnaire questionnaire={createQ} />
+
+      <div>
+        <Button href="./Grafica">Grafica tu questionario</Button>
+        <Doughnut data={data} />;
+      </div>
+
+
     </Container></>
     
   );
--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ const router = createBrowserRouter([
     path: "/AnswerQuestionnaire",
     element: <AnswerQuestionnaire/>,
   },
+ 
 ]);
 
 
